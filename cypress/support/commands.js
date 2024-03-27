8e29003462afbd4076af0995642b20431b0ee383
mazin93 <34219352+mazinassi@users.noreply.github.com>
2024-03-27 13:38:29 +0200

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,37 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginToYourstore',(email,password)=>{
+    cy.visit("/login/");
+    cy.get('#Email').clear()
+    cy.get('#Password').clear()
+    cy.get("#Email").type(email)
+    cy.get('#Password').type(password)
+    cy.get('.button-1').click();
+})
